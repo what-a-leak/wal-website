c04c538b40c79556b98790d745c0c244f823e801
Cyril Vasseur <vsr.cyril@gmail.com>
2024-11-20 11:33:36 +0100

    New version of plotting graph

--- demo/src/main/java/com/example/demo/HelloWorldController.java
+++ demo/src/main/java/com/example/demo/HelloWorldController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloWorldController {
 
     // Page d'accueil après connexion réussie
-    @GetMapping("/home")
+    @GetMapping("/home") 
     public String homePage(Model model) {
-        model.addAttribute("message", "Bienvenue sur What a Leak!");
-        return "hello";  // Renvoie vers hello.html
+        model.addAttribute("message", "Welcome on What a Leak!");
+        return "hello"; // Renvoie vers hello.html
     }
 }
